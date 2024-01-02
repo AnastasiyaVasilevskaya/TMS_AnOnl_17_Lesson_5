ace2f9fc90f5e70b4604224a8364eca79a80696a
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-01-02 19:59:23 +0300

    task 0. changing the variable size

--- src/IncreasingElements.java
+++ src/IncreasingElements.java
@@ -4,18 +4,14 @@
 //задается из консоли.
 
 import java.util.Scanner;
-
-
 public class IncreasingElements {
     public static void main(String[] args) {
-        int xSize = 4;
-        int ySize = 4;
-        int zSize = 4;
-        int[][][] array = new int[xSize][ySize][zSize];
+        int size = 4;
+        int[][][] array = new int[size][size][size];
 
-        for (int x = 0; x < xSize; x++) {
-            for (int y = 0; y < ySize; y++) {
-                for (int z = 0; z < zSize; z++) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                for (int z = 0; z < size; z++) {
                     array[x][y][z] = (int) (Math.random() * 10);
                     System.out.print(array[x][y][z] + " ");
                 }
@@ -29,9 +25,9 @@ public class IncreasingElements {
         int increaseNumber = scanner.nextInt();
 
         System.out.println("Массив после увеличения: ");
-        for (int x = 0; x < xSize; x++) {
-            for (int y = 0; y < ySize; y++) {
-                for (int z = 0; z < zSize; z++) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                for (int z = 0; z < size; z++) {
                     System.out.print(array[x][y][z] + increaseNumber + " ");
                 }
                 System.out.println();
